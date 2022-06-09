d511ea972b64038e2f63d1cae2aa22e1eb10a8c5
masnurrm <nurmuhammad448@gmail.com>
2022-06-09 08:39:20 +0700

    feat: kasir page

---
+++ src/resources/views/kasir/addproduct.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/addproduct.css">
+    <title>KULAKU</title>
+    <font></font>
+  </head>
+  <body>
+    <div class="div">
+      <div class="div-2">
+        <div class="builder-columns div-3">
+          <div class="builder-column column">
+            <div class="div-4">
+              <div class="div-5"></div>
+              <div class="div-6">
+                <picture>
+                  <img
+                    loading="lazy"
+                    src="img/logo-white.png"
+                    class="image"
+                  />
+                </picture>
+                <div class="builder-image-sizer image-sizer"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="div-7">
+        <div class="div-8">
+          <picture>
+            <img
+              loading="lazy"
+              src="img/back.png"
+              class="image"
+            />
+          </picture>
+        </div>
+        <div class="div-9">Tambah Produk</div>
+      </div>
+      <div class="div-10">
+        <div class="div-11">Nama</div>
+        <div class="div-12"></div>
+      </div>
+      <div class="div-13">
+        <div class="div-14">Kategori</div>
+        <div class="div-15">Pilih Kategori</div>
+      </div>
+      <div class="div-16">
+        <div class="div-17">Harga Beli</div>
+        <div class="div-18"></div>
+      </div>
+      <div class="div-19">
+        <div class="div-20">Harga Jual</div>
+        <div class="div-21"></div>
+      </div>
+      <div class="div-22">
+        <div class="div-23">Diskon</div>
+        <div class="div-24">0</div>
+      </div>
+      <div class="div-25">
+        <div class="div-26">Stok</div>
+        <div class="div-27">0</div>
+      </div>
+      <div class="div-28">
+        <div class="div-29">Foto</div>
+        <div class="div-30">Upload foto</div>
+      </div>
+      <div class="div-31"></div>
+      <div class="div-32"></div>
+    </div>
+  </body>
